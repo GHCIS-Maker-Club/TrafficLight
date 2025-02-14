99f1575af44a0d2d118ec001eb166360455df1a9
Sam <79125867+Sam-SSTU@users.noreply.github.com>
2025-02-14 12:22:43 +0800

    Update main.cpp

--- 2560/src/main.cpp
+++ 2560/src/main.cpp
@@ -1,12 +1,45 @@
-#include <Arduino.h>
+// testcolors demo for Adafruit RGBmatrixPanel library.
+// Renders 512 colors on our 16x32 RGB LED matrix:
+// http://www.adafruit.com/products/420
+// Library supports 4096 colors, but there aren't that many pixels!  :)
 
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <RGBmatrixPanel.h>
+
+// Most of the signal pins are configurable, but the CLK pin has some
+// special constraints.  On 8-bit AVR boards it must be on PORTB...
+// Pin 8 works on the Arduino Uno & compatibles (e.g. Adafruit Metro),
+// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
+// on the same PORT as the RGB data pins (D2-D7)...
+// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
+// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
+// Matrix Shield, cut trace between CLK pads and run a wire to A4).
+
+//#define CLK  8   // USE THIS ON ARDUINO UNO, ADAFRUIT METRO M0, etc.
+//#define CLK A4 // USE THIS ON METRO M4 (not M0)
+#define CLK 11 // USE THIS ON ARDUINO MEGA
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 void setup() {
-  Serial.begin(9600);
+  matrix.begin();
+  
+  // Fill entire 16x16 matrix with red color
+  for(uint8_t x=0; x<16; x++) {    // 16x16的屏幕
+    for(uint8_t y=0; y<16; y++) {
+      matrix.drawPixel(x, y, matrix.Color333(7, 0, 0)); // 最大红色(7)，无绿色(0)，无蓝色(0)
+    }
+  }
 }
 
 void loop() {
-  Serial.println("Hello, World!");
-  delay(1000);
+  // Do nothing -- image doesn't change
 }
-
